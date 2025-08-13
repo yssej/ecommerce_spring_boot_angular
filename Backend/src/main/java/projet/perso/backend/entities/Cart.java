2cf1a91ea5c47382bd5cf36006397d7665df6bbc
Jessy <jessyowen48@gmail.com>
2025-08-13 14:19:12 +0300

    create Cart & CartItem classes

---
+++ Backend/src/main/java/projet/perso/backend/entities/Cart.java
@@ -0,0 +1,25 @@
+package projet.perso.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+}
---
+++ Backend/src/main/java/projet/perso/backend/entities/CartItem.java
@@ -0,0 +1,33 @@
+package projet.perso.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "sub_total")
+    private BigDecimal subTotal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
