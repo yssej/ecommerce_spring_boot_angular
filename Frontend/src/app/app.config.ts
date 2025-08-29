bbca80b878c18c76b7f9b96fbf7b5dc65ddb1d0a
Jessy <jessyowen48@gmail.com>
2025-08-29 10:54:54 +0300

    recreate angular project

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,12 +1,5 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+  providers: []
 };
